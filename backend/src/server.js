438fb2b94efebc9d93b7f9eb59977b41cd560053
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-06-29 15:53:29 -0300

    Aumentando a segurança usando um arquivo .env para gerenciar informações do ambiente que não vão para o Github

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,2 +1,6 @@
 const app = require("./app");
-app.listen(3333, () => console.log("Server running on port 3333"));
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
