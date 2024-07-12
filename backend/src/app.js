27d0658fffa51d78999aaa99de7aa4af660bd2e0
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-12 14:46:11 -0300

    Adicionando o cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const router = require("./routes");
 
 const app = express();
 
 app.use(express.json());
-app.use(router); 
+app.use(cors);
+app.use(router);
 
 module.exports = app;
