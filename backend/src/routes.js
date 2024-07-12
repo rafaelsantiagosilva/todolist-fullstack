90764394bc94d356d27d0312e359e29127ce5b7e
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-12 16:46:44 -0300

    Consertando erro do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ const tasksMiddleware = require("./middlewares/TasksMiddleware");
 
 const router = express.Router();
 
-router.get('/tasks', (req, res) => { taskController.getAll(req, res) });
+router.get('/tasks', taskController.getAll);
 router.post('/tasks', tasksMiddleware.validateFieldTitle, taskController.createTask);
 router.delete('/tasks/:taskId', taskController.deleteTask);
 router.put('/tasks/:taskId', tasksMiddleware.validateFieldTitle, tasksMiddleware.validateFieldStatus, taskController.updateTask);
