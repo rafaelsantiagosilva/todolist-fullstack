742a68092b68c0de0e219e24887f4080dfd1b64e
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-12 12:00:17 -0300

    Iniciando o backend da aplicação

--- backend/src/models/tasksModel.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection');
-
-async function getAll() {
-     return await connection.execute('SELECT * FROM tasks');
-}
-
-module.exports = {
-     getAll
-};
--- backend/src/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const tasksController = require('./controllers/tasksController');
-
-const router = express.Router();
-
-router.get("/tasks", (req, res) => tasksController.getAll(req, res));
-
-module.exports = router;
